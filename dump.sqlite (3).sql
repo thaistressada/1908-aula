f89f9e329f637e2e013c6eef428de6ee314b5275
thaistressada <159055013+thaistressada@users.noreply.github.com>
2024-08-19 14:01:04 -0300

    Add files via upload

---
+++ dump.sqlite (3).sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE produtos(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  descricao TEXT,
+  preco DECIMAL(10,2),
+  dataHora DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
